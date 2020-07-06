37232fc9f7dc659ea4aa8fac86e7d5ebb3deae67
aoki <ringohub@gmail.com>
2020-07-07 01:58:56 +0900

    Add theme context sample

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../styles.css";
 import TitleBar from "../components/title-bar";
+import { ThemeContext, useTheme } from "../contexsts/theme-context";
 
 const MyApp = ({ Component, pageProps }) => {
+  const theme = useTheme();
+  console.log(`Render ${theme.theme.name}`)
+
   return (
-    <div className="main">
-      <TitleBar />
-      <Component {...pageProps} />
-      <style jsx>{`
-        .main {
-          display: flex;
-          flex-direction: column;
-          min-height: 100vh;
-        }
-      `}</style>
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <div className="main">
+        <TitleBar {...pageProps} />
+        <Component {...pageProps} />
+        <style jsx>{`
+          .main {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: ${theme.theme.background};
+            color: ${theme.theme.color};
+            transition: 0.5s;
+          }
+        `}</style>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,16 +1,21 @@
 import Head from "next/head";
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../contexsts/theme-context";
 
 const IndexPage = () => {
+  const themeContext = useContext(ThemeContext)
+  const clickHandler = () => {
+    console.log("Click")
+    themeContext.setTheme(themeContext.theme.name === "light" ? "dark" : "light")
+  }
+
   return (
-    <div className="main">
+    <div className="main" onClick={clickHandler}>
       <Head>
         <title>Hello Nextron</title>
       </Head>
       <div>
-        <img src="/images/logo_electron.svg" id="electron-log" />
-        <h1> Electron + Next.js</h1>
-        <img src="/images/logo_nextjs.svg" id="nextjs-log" />
+        <h1> Electron + Next.js </h1>
       </div>
       <style jsx>{`
         .main {
@@ -20,16 +25,11 @@ const IndexPage = () => {
           flex-grow: 1;
         }
         h1 {
+          user-select: none;
           font-weight: 100;
           display: inline;
           margin-right: 0.5em;
           margin-left: 0.5em;
-        }
-        #electron-log {
-          height: 24px;
-        }
-        #nextjs-log {
-          height: 24px;
         }
       `}</style>
     </div>
